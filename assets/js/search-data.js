9a7d291db9b29d3bede123fd77f2f747bb355398
apoupard-ntr <apoupard@parisnanterre.fr>
2026-05-05 15:08:29 +0000

    Deploying to gh-pages from @ circee-ecol-econ/circee-ecol-econ.github.io@678115525b8886da4e69a109823cebef1c2450c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-calendrier",
-    title: "Calendrier",
+    id: "nav-accueil",
+    title: "Accueil",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -15,6 +15,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/about/";
+          },
+        },{id: "nav-calendrier",
+          title: "Calendrier",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/calendar/";
           },
         },{id: "post-séance-métaux-critique-et-sud-s",
         
